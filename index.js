a82cf5deff6051c2640bef5d07f55c1a7e2dd9e2
Brian Takita <brian.takita@gmail.com>
2013-02-09 23:24:12 -0800

    (express-http-routes).attach takes a params hash, which takes app and an optional override handler function. Removed depnendency on underscore.js.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ module.exports.all = {};
 module.exports.route = function(routeDefinition) {
   return module.exports.all[routeDefinition];
 };
-module.exports.attach = function(app) {
-  _.each(this.all, function(routeHandler, routeDefinition) {
+module.exports.attach = function(params) {
+  for (var routeDefinition in this.all) {
+    var routeHandler = this.all[routeDefinition];
     var routeParts = routeDefinition.split(" ");
     var routeVerb = routeParts[0].toLowerCase();
-    var routePath = _.rest(routeParts).join(" ");
+    var routePath = routeParts.slice(1).join(" ");
 
-    app[routeVerb](routePath, routeHandler);
-  }, module.exports);
+    var routeHandlerOverride = params.override ?
+      params.override(routeDefinition, routeHandler) :
+      routeHandler;
+    params.app[routeVerb](routePath, routeHandlerOverride);
+  }
 };
